2145274d029ad0fc3fe321f5a245d3631e70e82c
DiMaPaGa <dianamariapascual@gmail.com>
2023-09-29 20:14:56 +0200

    clase29092023

---
+++ PROGRAMACION/Primer Proyecto 1copiado mal/src/com/company/Boletin1.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Boletin1 {
+
+    public static void main (String[]args){
+
+ // 1. Imprime por pantalla tu nombre.
+
+        System.out.println("Diana");
+
+//2. Imprime por pantalla tu nombre y dos apellidos, cada uno en una línea.
+
+//3.a Repite el ejercicio anterior pero imprimiendo todo en la misma línea.
+
+/*3.b Repite el ejercicio anterior imprimiendo todo en la misma línea, usando
+tres instrucciones de impresión.*/
+
+//4. Imprime tu edad.
+
+//5. Imprime el primer digito de tu edad en una línea, y el segundo en otra.
+
+/*6. Imprime el primer digito de tu edad. Tras ello y con una nueva instrucción,
+imprime el segundo. Tienen que aparecer ambos en la misma línea.*/
+
+/*7. Imprime al revés tu nombre, de forma que cada letra aparezca en una línea.
+Por ejemplo, en mi caso (Jorge) la salida del programa sería:
+e
+g
+r
+o
+J*/
+
+/*8. Repite lo anterior, pero entre cada letra debe aparecer una línea en blanco.
+
+Por ejemplo, en mi caso (Jorge) la salida del programa sería:
+e
+
+g
+
+r
+
+o
+
+J*/
+
+/*9. Imprime por pantalla tu nombre concatenándolo con tu edad, de forma que quede
+un espacio en blanco entre ambos.*/
+
+/*10. Imprime por pantalla el número 3 concatenándolo con el número 7. Comprueba
+si tu compañero lo ha hecho igual que tú.*/
+
+/*11. Imprime por pantalla la cadena "Hola mundo" y a continuación, la cadena
+"Hola\nmundo". ¿Para qué sirve el '\n'? ¿Puede utilizarse varias veces seguidas?*/
+
+//12. ¿Para qué sirve el operador '+'?
+
+
+
+
+
+    }//main
+
+}//class
--- PROGRAMACION/Primer Proyecto/src/com/company/Main.java
+++ PROGRAMACION/Primer Proyecto/src/com/company/Main.java
@@ -1,25 +1,42 @@
 package com.company;
 
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce tu edad: ");
+        int edadUsuario= scanner.nextInt();
+        System.out.println("Tu edad es: "+ edadUsuario);
+        System.out.print("Introduce tu peso: ");
+        int pesoUsuario= scanner.nextInt();
+        System.out.println("Tu peso es: "+ pesoUsuario);
+        System.out.print("Introduce tu altura: ");
+        float alturaUsuario= scanner.nextFloat();
+        System.out.println("Tu altura es: "+ alturaUsuario);
+        System.out.print("Introduce tu CI: ");
+        int CIUsuario= scanner.nextInt();
+        System.out.println("Tu CI es: "+ CIUsuario);
+        System.out.println("FIN");
 	// Comenzamos...Comentario de una línea.
 
         /*
         Probamos comentario que afecta a diferentes
         lineas.
          */
-int edadDiana = 39;
+/*int edadDiana = 39;
 int edadMoises = 39;
 boolean estaLloviendo=false;
 boolean somosRicos=true;
 float precio = 1.89f;
-double lat = 23.598741;
+double lat = 23.598741;*/
 
 //Actividad
 
-        int edadJorge =39;
+        /*int edadJorge =39;
         float alturaDiana= 1.76f;
         boolean Esrepetidor= true;
         double resultado= 1.58459763;
@@ -32,6 +49,25 @@ double lat = 23.598741;
         char letra2Matricula= 'C';
         char letra3Matricula= 'D';
 
+System.out.println ("Mi edad es: "+ 39);
+System.out.println (10+6);
+System.out.println (edadDiana + 6);
+System.out.println ("edad" + (6 + 9));
+System.out.println ("edad" + 6 + 9);
+System.out.println (3+((4/2)*5)-1);
+System.out.println (4.0f/3);Carga solo los enteros si no tiene nada decimal (4/3).
+Tal y como está, ya lo convierte en float, pero también hubiera valido poner (4f/3)
+También vale hacer un "castin" como a continuaciónSystem.out.println((float)4)/3;*/
+
+/*INCREMENTOS
+*
+* edadJorge++;
+* edadJorge +=2; dará 41
+* edadJorge -=2; dará 37
+* edadJorge *=2; dará 58
+* edadJorge /=2; si lo convierto en float, dará la mitad.
+*
+* */
 
 
     }//main
