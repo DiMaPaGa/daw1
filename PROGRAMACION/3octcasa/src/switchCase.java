c94b6a594c6e69c5a5d9f8f85ad7811648f7b37a
DiMaPaGa <dianamariapascual@gmail.com>
2023-10-16 14:08:36 +0200

    Dia16casa

--- PROGRAMACION/3octcasa/src/switchCase.java
+++ PROGRAMACION/3octcasa/src/switchCase.java
@@ -38,7 +38,7 @@ public class switchCase {
         /*7. Leer seis números y comprobar si coinciden con la combinación ganadora de la
         primitiva (1, 3, 10, 20, 33, 40), imprimiendo un mensaje al respecto.*/
 
-        System.out.println("Incorpora el primer número");
+      /*  System.out.println("Incorpora el primer número");
         int num1 = scan.nextInt();
         System.out.println("Incorpora el segundo número");
         int num2 = scan.nextInt();
@@ -49,7 +49,7 @@ public class switchCase {
         System.out.println("Incorpora el quinto número");
         int num5 = scan.nextInt();
         System.out.println("Incorpora el sexto número");
-        int num6 = scan.nextInt();
+        int num6 = scan.nextInt();*/
 
         /*if (num1 == 1) {
             System.out.println("Coincide el primer número" + num1);
@@ -68,17 +68,17 @@ public class switchCase {
         }
         if (num6 == 44) {
             System.out.println("Coincide el sexto número " + num6);*/
-        if (num1 == 1 && num2 == 3 && num3 == 10 && num4 == 20 && num5 == 33 && num6 == 40) {
+       /* if (num1 == 1 && num2 == 3 && num3 == 10 && num4 == 20 && num5 == 33 && num6 == 40) {
             System.out.println("¡¡Te ha tocado la lotería!!");
         } else {
             System.out.println("No te ha tocado la lotería, lo siento");
-        }
+        }*/
 
        /* 8. Modificar el ejercicio anterior para que, al leerlos, se compruebe si todos los
         números leídos tienen el rango apropiado (entre 1 y 49). Si no lo tienen, imprimir un
         mensaje de error y el programa acabará.*/
         //solución del profesor
-        boolean num1Valido= (num1>=1 && num1<=49);
+      /*  boolean num1Valido= (num1>=1 && num1<=49);
         boolean num2Valido= (num2>=1 && num2<=49);
         boolean num3Valido= (num3>=1 && num3<=49);
         boolean num4Valido= (num4>=1 && num4<=49);
@@ -93,7 +93,7 @@ public class switchCase {
             }
         } else {
             System.out.println("Error, algún número no se ajusta a los valores permitidos");
-        }
+        }*/
 
        /* if ((num1 < 1 || num1> 49) || (num2 < 1 || num2 > 49) || (num3 < 1 || num3 > 49) || (num4 < 1 || num4 > 49) || (num5 < 1 || num5 > 49) || (num6 < 1 || num6 > 49)) {
             System.out.println("Error, algún número no se ajusta a los valores permitidos");
@@ -105,21 +105,21 @@ public class switchCase {
             }*/
             //9.  Leer por pantalla un entero y comprobar si es par o impar (use el operador módulo%).
 
-            System.out.println("Incorpora un número");
+           /* System.out.println("Incorpora un número");
             int parOimpar = scan.nextInt();
 
             if (parOimpar%2 == 0) {
                 System.out.println("El número ingresado es par");
             } else {
                 System.out.println("el númer ingresado es impar");
-            }
+            }*/
 
       /* 10. Crear un programa que calcule el índice de masa corporal. Lea los datos que sean
         necesarios, imprima por pantalla el índice, y dependiendo de su valor imprima al
         usuario un mensaje informativo. Por ejemplo: está por debajo de lo normal, está
         normal o está por encima de lo normal.*/
 
-            System.out.println("Indica cuánto pesas en kilogramos");
+           /* System.out.println("Indica cuánto pesas en kilogramos");
             float peso = scan.nextFloat();
             System.out.println("Indica cuánto mides en metros");
             float tuAltura = scan.nextFloat();
@@ -136,13 +136,13 @@ public class switchCase {
             }
             if (IMC >= 30.0) {
                 System.out.println("Tienes un IMC de obesidad");
-            }
+            }*/
 
        /* 11. Leer por pantalla un número entre 1 y 12 e imprimir por pantalla el nombre del mes
         correspondiente. En caso de leer un número fuera de rango, imprimir por pantalla un
         mensaje de error.*/
 
-            System.out.println("Ingresa el número del mes");
+         /*   System.out.println("Ingresa el número del mes");
             int mes = scan.nextInt();
 
             switch (mes) {
@@ -185,7 +185,7 @@ public class switchCase {
                 default:
                     System.out.println("Identficador no válido");
                     break;
-            }
+            }*/
 
 
           /*12. Establezca un número entre 0 y 9999, que servirá como contraseña. Puede elegir el
@@ -193,7 +193,7 @@ public class switchCase {
           accediendo al sistema”, en caso contrario escriba “Contraseña incorrecta, cerrando
           sesión”.*/
 
-            int codigo;
+          /*  int codigo;
             codigo = 7777;
             System.out.println("Inserte contraseña");
 
@@ -202,7 +202,7 @@ public class switchCase {
             } else {
                 System.out.println("Contraseña incorrecta, cerrando sesión");
             }
-
+*/
 
 
         /*13. Desarrolle un sistema de cálculo de descuentos para una farmacia. Cuando el
@@ -210,17 +210,17 @@ public class switchCase {
         valor, e imprimir por pantalla el precio final. Recuerde que debe imprimir el precio
         final tras aplicar el descuento, no la cantidad que se descuenta.*/
 
-            System.out.println("Introduzca el precio del producto");
+          /*  System.out.println("Introduzca el precio del producto");
             float precioProduc = scan.nextFloat();
             float precioConDescuento = (precioProduc * 0.75f);
             float cantidadDescontada= precioProduc-precioConDescuento;
             System.out.println("El precio del producto aplicando descuento es: " + precioConDescuento );
-            System.out.println("La cantidad descontada ha sido de: "+ cantidadDescontada);
+            System.out.println("La cantidad descontada ha sido de: "+ cantidadDescontada);*/
 
         /* 14. Modifique el anterior para que además se imprima la cantidad que se descuenta.*/
         /*15. Diseñe un programa que lea la temperatura en centígrados del día e imprima el tipo
         de clima de acuerdo a la siguiente tabla.*/
-        System.out.println("Introduzca la temperatura en centígrados");
+      /*  System.out.println("Introduzca la temperatura en centígrados");
         int temperaturaClima = scan.nextInt();
         if (temperaturaClima<=10){
             System.out.println("Hoy hace frío");
@@ -233,11 +233,206 @@ public class switchCase {
         }
         else {
             System.out.println("Hoy la temperatura es tropical");
-        }
-/* Pedir un número entre 0 y 99999 y decir cuántas cifras tiene. Suponga que se
+        }*/
+/* Act.16. Pedir un número entre 0 y 99999 y decir cuántas cifras tiene. Suponga que se
 introducirán números en ese rango.*/
+    /*    System.out.println("Ingrese un número de 0 a 99999");
+        int numero = scan.nextInt();
+
+        if (numero>=0 && numero<10){
+            System.out.println("Su número tiene 1 cifra");
+        } else if (numero>=10 && numero<100) {
+            System.out.println("Su número tiene 2 cifras");
+        } else if (numero>=100 && numero<1000) {
+            System.out.println("Su número tiene 3 cifras");
+        } else if (numero>=1000 && numero<10000) {
+            System.out.println("Su número tiene 4 cifras");
+        } else if (numero>=10000 && numero<100000) {
+            System.out.println("Su número tiene 5 cifras");
+        } else {
+            System.out.println("Identficador no válido");
+        }*/
+/*17. La universidad ha categorizado las matrículas de acuerdo a la facultad que va a
+estudiar el postulante. Ingrese por teclado el nombre de la facultad donde va a
+estudiar, y muestre el importe y la mensualidad. (Use el control switch-case, y
+recuerde que el nombre que introduzca debe coincidir exactamente con el nombre
+de universidad).*/
+
+     /*   System.out.println("Ingrese su facultad según los siguientes identificadores: Sistemas, Derecho, Naviera, Pesquera, Contabilidad o Administración");
+        String identificador = scan.next();
+
+        switch (identificador) {
+            case "Sistemas":
+                System.out.println("Importe de matricula: 350€, mensualidad: 650€");
+                break;
+            case "Derecho":
+                System.out.println ("Importe de matricula: 300€, mensualidad: 550€");
+                break;
+            case "Naviera":
+                System.out.println ("Importe de matricula: 300€, mensualidad: 500€");
+                break;
+            case "Pesquera":
+                System.out.println ("Importe de matricula: 310€, mensualidad: 460€");
+                break;
+            case "Contabilidad":
+                System.out.println ("Importe de matricula: 280€, mensualidad: 490€");
+                break;
+            case "Administración":
+                System.out.println ("Importe de matricula: 360€, mensualidad: 520€");
+                break;
+            default:
+                System.out.println("Identificador no válido. Revise que el nombre indicado se ajuste a las opciones indicadas.");
+                break;
+        }*/
+
+/*18. Escriba programa para ingresar 4 notas de un alumno, calcular y mostrar su
+promedio, sabiendo que la primera y segunda tiene 20% de peso cada una, la
+tercera y cuarta tienen 30% de peso cada una.*/
+
+        System.out.println("Ingrese su primera nota");
+        float nota1= scan.nextFloat();
+
+        System.out.println("Ingrese su segunda nota");
+        float nota2= scan.nextFloat();
+
+        System.out.println("Ingrese su tercera nota");
+        float nota3= scan.nextFloat();
+
+        System.out.println("Ingrese su cuarta nota");
+        float nota4= scan.nextFloat();
+
+        float notaFinal= (nota1*0.2f)+(nota2*0.2f)+(nota3*0.3f)+(nota4*0.3f);
+        System.out.println("Tu nota final es "+ notaFinal);
 
 
+/* 19. Desarrolle un programa que pida cuantos hombres y mujeres hay en un aula y
+permita calcular el porcentaje de hombres y mujeres.*/
+
+        System.out.println("¿Cuántos hombres hay en el aula?");
+        int hombres= scan.nextInt();
+        System.out.println("¿Cuántas mujeres hay en el aula?");
+        int mujeres= scan.nextInt();
+        int total= hombres+mujeres;
+        System.out.println("En la clase, el "+ ((hombres*100f)/total) + "% son hombres y el " + ((mujeres*100f)/total)+ "% son mujeres");
+
+        float porcHombres= (hombres*100f)/total;
+        float porcMujeres= (mujeres*100f)/total;
+
+        System.out.println("En la clase, el "+ porcHombres + "% son hombres y el " + porcMujeres+ "% son mujeres");
+
+
+// 21. Leer por pantalla dos números enteros, e intercambiar sus valores (swapping).
+
+        System.out.println("Introduzca un número entero");
+        int valor1= scan.nextInt();
+        int valor2= valor1;
+
+        System.out.println("Introduzca otro número entero");
+        valor1= scan.nextInt();
+
+        System.out.println("Intercambiamos y ahora el valor1 vale "+ valor1 + " y el valor2 vale "+ valor2);
+
+//22. Leer por pantalla dos números enteros, e imprimirlos por pantalla en orden creciente.
+
+        System.out.println("Introduzca un número entero");
+        int valor3= scan.nextInt();
+
+        System.out.println("Introduzca otro número entero");
+        int valor4= scan.nextInt();
+
+        if (valor3>valor4) {
+            System.out.println (valor3+ " es mayor que "+ valor4);
+        } else if (valor3<valor4){
+            System.out.println (valor4+ " es mayor que "+ valor3);
+        } else {
+            System.out.println("Ambos números son iguales. Su valor es: "+ valor3);
+        }
+
+
+/*23. Leer por pantalla tres números enteros, e imprimirlos por pantalla en orden
+creciente.*/
+
+        System.out.println("Introduzca números diferentes entre sí. Introduzca un número entero");
+        int valor5= scan.nextInt();
+
+        System.out.println("Introduzca otro número entero");
+        int valor6= scan.nextInt();
+
+        System.out.println("Introduzca el último número entero");
+        int valor7= scan.nextInt();
+        int mayor;
+        int menor;
+        int mediano;
+
+        if ((valor5 > valor6) && (valor5 > valor7)) {
+            mayor=valor5;
+
+        } else if ((valor6 > valor5) && (valor6 > valor7)) {
+            mayor=valor6;
+        } else {
+            mayor=valor7;
+        }
+        if ((valor5 < valor6) && (valor5 < valor7)) {
+            menor=valor5;
+        } else if ((valor6 < valor5) && (valor6 < valor7)) {
+            menor=valor6;
+        } else {
+            menor=valor7;
+        }
+
+        if ((valor5!=mayor && valor5!=menor)) {
+            mediano=valor5;
+        } else if ((valor6!=mayor && valor6!=menor)) {
+            mediano=valor6;
+        } else {
+            mediano=valor7;
+        }
+
+        System.out.println("El orden creciente sería, primero  "+ menor + " después "+ mediano + " y por último " + mayor);
+//24. Repetir el ejercicio anterior, e imprimirlos en orden decreciente.
+
+        System.out.println("El orden decreciente sería, primero  "+ mayor + " después "+ mediano + " y por último " + menor);
+
+
+/*25. Lea un número por pantalla y determine si es múltiplo de 10 (use el operador módulo
+//%).*/
+
+        System.out.println("Introduzca un número");
+        int numEjercicio25= scan.nextInt();
+
+        if ((numEjercicio25%10==0)) {
+            System.out.println("El número introducido es múltiplo de 10");
+        } else {
+            System.out.println("El número introducido NO es múltiplo de 10");
+        }
+
+/*26. Leer por pantalla un número de tres cifras, e imprimir cada una de sus cifras en una
+línea.*/
+
+
+/*27. Crea una aplicación llamada CalculadoraInversa, que lea por pantalla dos operandos
+(int) y un signo aritmético (String), y según este último se realizará la operación
+correspondiente. Al final mostrará el resultado en consola. Los signos aritméticos
+disponibles son:
++: suma los dos operandos.
+-: resta los operandos.
+*: multiplica los operandos.
+/: divide los operandos, este debe dar un resultado con decimales (double)
+%: módulo, resto de la división entre operando1 y operando2.
+Para leer por pantalla usar nextInt y next.*/
+
+
+
+
+ /*28. En un casino de juegos se desea mostrar los mensajes respectivos por el puntaje
+obtenido en el lanzamiento de tres dados de un cliente, de acuerdo a los siguientes
+resultados:
+Si los tres dados son seis, mostrar el mensaje “Muy buena suerte”
+Si dos dados son seis, mostrar el mensaje “Buena suerte”
+Si un dado es seis, mostrar el mensaje “Mala Suerte”
+Si ningún dado se obtiene seis, mostrar el mensaje “Muy mala suerte”
+Simule los lanzamientos con tres números aleatorios, usando instrucciones Java que
+los genere.*/
 
         }//main
     }//class
