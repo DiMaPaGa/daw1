14389a2ebc64db8ba0e3b8224bf077af4a8007aa
DiMaPaGa <dianamariapascual@gmail.com>
2023-10-24 15:16:10 +0200

    Clase24oct

---
+++ PROGRAMACION/bASE DE DATOS/Actividades día 23 octubre.sql
@@ -0,0 +1,9 @@
+-- Encuentra todos los productos que tienen un precio unitario mayor a 20 y ordénalos por nombre de producto en orden ascendente.
+SELECT * FROM northwind.products where UnitPrice > 20  order by ProductName asc;
+-- Encuentra todas las categorías que tienen más de 5 productos con un precio unitario mayor a 20.Agrupa los resultados por categoría.
+select CategoryID, count(*) as "Recuento pedidos" from products  where UnitPrice>20 group by CategoryID having count(*)>5;
+-- Encuentra todos los clientes que han realizado más de 10 pedidos. Agrupa los resultadospor cliente y ordénalos por nombre en orden ascendente.
+SELECT CustomerID, Count(*) as "Recuento pedidos" FROM northwind.customers group by CustomerID having Count(*)>10 order by CustomerID Asc;
+-- Encuentra todas las categorías en las que el precio promedio de los productos es mayor a 20. Agrupa los resultados por categoría, utiliza HAVING para filtrar los grupos y ordénalos por nombre de categoría en orden ascendente.
+select CategoryID, count(*) as "Recuento pedidos" from products group by CategoryID having avg(UnitPrice)>20 order by CategoryID asc;
+-- Encuentra todos los empleados que han realizado más de 20 pedidos con un valor de transporte (freight) total mayor a 5000. Agrupa los resultados por empleado, utiliza HAVING para filtrar los grupos y ordénalos por apellido del empleado en orden ascendente.SELECT LastName, count(*) as "Total de pedidos", sum(Freight) as "Total Freight" FROM northwind.orders JOIN employees ON EmployeeID = EmployeeID group by EmployeeID, LastName Having "Total de pedidos">20 and "Total Freight" > 5000 order by LastName Asc;
