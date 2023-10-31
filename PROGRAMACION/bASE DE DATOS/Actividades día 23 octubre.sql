79c13e1f35014d09cf210a2b455a3457156a2a03
DiMaPaGa <dianamariapascual@gmail.com>
2023-10-31 12:40:34 +0100

    31OctCasa

--- PROGRAMACION/bASE DE DATOS/Actividades día 23 octubre.sql
+++ PROGRAMACION/bASE DE DATOS/Actividades día 23 octubre.sql
@@ -14,3 +14,24 @@ SELECT CustomerID, count(*) from orders orderdetails where Freight>50 group by C
 SELECT OrderID, count(*) as "recuentoProductos" FROM northwind.orderdetails where Discount>0.0 group by OrderID having recuentoProductos>2 order by recuentoProductos desc;
 -- Haz el recuento de clientes agrupados por ciudad y pais, mostrando únicamente aquellos casos en los que se cuente con más de dos clientes. Ordénalos por países en orden alfabético.
 SELECT City, Country, Count(*) FROM northwind.customers group by City, Country having Count(*)>2 order by Country asc;
+-- continuación actividades boletín 5 de josé carlos:
+-- 23.Por cada producto muestre el nombre de producto, longitud en caracteres del nombre de producto, precio unitario redondeado al entero inferior más cercano,
+-- número de unidades en stock y ganancia. La ganancia de cada producto se calcula utilizando el número de unidades del producto y el precio unitario redondeado de
+-- dicho producto.
+SELECT ProductName, char_length(ProductName) as longitud_nombre, floor(UnitPrice) as "Precio redondeado", UnitsInStock, floor(UnitPrice) * floor(UnitsInStock) as "Ganancia" FROM northwind.products;
+-- 24. Cuente la cantidad de productos que pertenecen a la categoría “Bebidas”.
+SELECT CategoryID, count(*) FROM northwind.products group by CategoryID having CategoryID=1;
+SELECT CategoryID, count(*) FROM northwind.products where CategoryID=1;
+-- 25. Por cada producto muestre el nombre de producto, nombre de la compañía proveedora y ciudad de la compañía proveedora, para aquellos productos sin stock. (JOIN)
+
+-- 26. Muestre los proveedores con algún valor visible en el campo Homepage.
+SELECT * FROM northwind.suppliers where HomePage is not null and HomePage<>(" ");
+-- 27. Halle el total de unidades en stock para aquellos productos cuya descripción de categoría es “Carnes preparadas”.
+SELECT sum(UnitsInStock) as "NÚMERO CARNES PREPARADAS EN STOCK" FROM northwind.products where CategoryID=6;
+-- 28. Por cada cliente halle la cantidad total de pedidos, sólo para aquellos clientes de Alemania, Canada o Venezuela que tienen algún valor visible en el campo Region.
+-- Este no: SELECT CustomerID, count(*) FROM northwind.orders WHERE ShipRegion<>(" ") and ShipRegion IS NOT NULL and ShipCountry="Germany" or ShipCountry="Venezuela" or ShipCountry="Canada" group by CustomerID;
+SELECT CustomerID, count(*) FROM northwind.orders WHERE ShipCountry IN ("Germany","Venezuela","Canada") and ShipRegion<>" " and ShipRegion IS NOT NULL group by CustomerID;
+-- 29. Por cada cliente, pedido realizado y empleado asociado, muestre los distintos productos (id de producto). (JOIN)
+
+-- 30. Halle la edad media de los representantes de ventas de Londres y Redmond que sean mujeres.
+SELECT avg(timestampdiff(year,BirthDate, Curdate())) as "media de edad" FROM northwind.employees where City in ("London", "Redmon") and Title="Sales Representative" and TitleOfCourtesy in ("Ms.","Mrs.");
