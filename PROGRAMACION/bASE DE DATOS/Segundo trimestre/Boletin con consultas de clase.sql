b8f0c60570204df6179a4f1ba5a5f88ca2c96dea
DiMaPaGa <145446082+DiMaPaGa@users.noreply.github.com>
2024-01-19 14:02:11 +0100

    casa19

--- PROGRAMACION/bASE DE DATOS/Segundo trimestre/Boletin con consultas de clase.sql
+++ PROGRAMACION/bASE DE DATOS/Segundo trimestre/Boletin con consultas de clase.sql
@@ -5,13 +5,18 @@ WHERE categories.CategoryID=products.CategoryID
 AND products.ProductName LIKE "J%";
 
 -- 2.Muestra el nombre, la edad de los empleados,el nombre de producto,su precio y stock de las ordenes mayores de 10250
-SELECT employees.FirstName, timestampdiff(year,employees.BirthDate, curdate()), products.ProductName, products.UnitPrice, products.UnitsInStock
-FROM employees, products
-WHERE products.
+SELECT employees.FirstName, timestampdiff(year,employees.BirthDate, curdate()) as "Edad_empleados", products.ProductName, products.UnitPrice, products.UnitsInStock
+FROM employees, products, orders, orderdetails
+WHERE employees.EmployeeID = orders.EmployeeID
+AND orders.OrderID = orderdetails.OrderID
+AND orderdetails.ProductID = products.ProductID
+AND orders.OrderID > 10250;
 
-
--- 3.Seleccionar cuantos proveedores hay en cada pais, considerando solo a los nombres de  los proveedores que comienzan con la letra 'E' hasta la 'P'
-
+-- 3.Seleccionar cuantos proveedores hay en cada pais, considerando solo a los nombres de los proveedores que comienzan con la letra 'E' hasta la 'P'
+SELECT suppliers.Country, count(*) as "Numero de proveedores"
+FROM suppliers
+WHERE suppliers.CompanyName between 'E%' and 'Pz%'
+GROUP BY Country;
 
 -- 4.Selecciona el codigo de producto, el nombre del producto, el nombre de categoria del pedido 10654
 
@@ -36,7 +41,19 @@ WHERE products.
 
 -- 11.Selecciona el ID del cliente, el nombre de contacto del cliente, el nombre del producto, el ID del pedido, la cantidad de los detalles del pedido y su precio unitario
 -- Muestra únicamente aquellos pedidos cuyo total (cantidad * precio unitario) sea superior a 5000.
+SELECT Customers.CustomerID, Customers.ContactName, Products.ProductName, Orders.OrderID, OrderDetails.Quantity, OrderDetails.UnitPrice
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+JOIN Products ON OrderDetails.ProductID = Products.ProductID
+WHERE (OrderDetails.Quantity * OrderDetails.UnitPrice) > 5000;
 
+SELECT Customers.CustomerID, Customers.ContactName, Products.ProductName, Orders.OrderID, OrderDetails.Quantity, OrderDetails.UnitPrice
+FROM customers, orders, orderdetails, products
+WHERE customers.CustomerID = orders.CustomerID
+AND orders.OrderID = orderDetails.OrderID
+AND orderDetails.ProductID = products.ProductID
+AND (OrderDetails.Quantity * OrderDetails.UnitPrice) > 5000;
 
 -- 12.Obtener el ID del empleado, el nombre y apellido del empleado en una sola columna, el ID del pedido, la fecha del pedido, el ID del cliente,el nombre de la compañía del cliente de las órdenes hechas en 1996 en Estados Unidos
 
@@ -47,6 +64,17 @@ WHERE products.
 
 -- 14.Selecciona el nombre y apellido del empleado (misma columna como "Nombre_Completo"), el nombre de contacto del cliente, el ID del pedido y la fecha del pedido
 -- Los resultados se ordenan por el Nombre_completo del empleado y la fecha del pedido.
+SELECT concat_ws(" ", employees.FirstName, Employees.LastName) as "Nombre_Completo", Customers.ContactName, Orders.OrderID, Orders.OrderDate
+FROM Employees
+JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+ORDER BY Nombre_Completo, Orders.OrderDate;
+
+SELECT concat_ws(" ", employees.FirstName, Employees.LastName) as "Nombre_Completo", Customers.ContactName, Orders.OrderID, Orders.OrderDate
+FROM employees, orders, customers
+WHERE employees.EmployeeID = orders.EmployeeID
+AND orders.CustomerID = customers.CustomerID
+ORDER BY Nombre_Completo, Orders.OrderDate;
 
 
 -- 15.Selecciona el codigo de empleado, la cantidad de productos y la fecha de venta de cada pedido ordenado de más reciente a más antiguo
