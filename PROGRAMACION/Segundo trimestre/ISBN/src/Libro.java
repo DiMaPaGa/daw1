f6d5d3035f1ab0bbe163270655045e3980e9601b
DiMaPaGa <145446082+DiMaPaGa@users.noreply.github.com>
2024-02-14 13:50:48 +0100

    casa14feb

--- PROGRAMACION/Segundo trimestre/ISBN/src/Libro.java
+++ PROGRAMACION/Segundo trimestre/ISBN/src/Libro.java
@@ -1,12 +1,12 @@
+import java.util.InputMismatchException;
+
 public class Libro {
 
     //Atributos
 
-    private long isbn;
-
+    private String isbn;
     private String titulo;
     private String autor;
-
     private int numeroPaginas;
     private String editorial;
 
@@ -17,23 +17,23 @@ public Libro(){
 
 }
 
-public Libro(long isbn, String titulo, String autor){
+public Libro(String isbn, String titulo, String autor){
  this();
- this.isbn=isbn;
- this.titulo=titulo;
- this.autor=autor;
+ setIsbn(isbn);
+ setTitulo(titulo);
+ setAutor(autor);
 
 
 }
 
-public Libro(long isbn, String titulo, String autor, int numeroPaginas, String editorial){
+public Libro(String isbn, String titulo, String autor, int numeroPaginas, String editorial){
  this (isbn, titulo, autor);
- this.numeroPaginas=numeroPaginas;
- this.editorial=editorial;
+ setNumeroPaginas(numeroPaginas);
+ setEditorial(editorial);
 
 }
 
-    public long getIsbn() {
+    public String getIsbn() {
         return isbn;
     }
 
@@ -53,23 +53,49 @@ public Libro(long isbn, String titulo, String autor, int numeroPaginas, String e
         return editorial;
     }
 
-    public void setIsbn(long isbn) {
+    public void setIsbn(String isbn) {
+    if (contieneNulo(isbn)) {
+        throw new NullPointerException("ISBN no válido");
+    }
+    if(isbn.length()<10 || isbn.length()>13){
+        throw new IllegalArgumentException ("El ISBN solo comprende de 10 a 13 dígitos");
+    }
+    if (!contieneDigitos(isbn)){
+        throw new InputMismatchException("el ISBN solo puede contener dígitos");
+    }
+
         this.isbn = isbn;
     }
 
+
     public void setTitulo(String titulo) {
+        if (contieneNulo(titulo)) {
+            throw new NullPointerException("Título no válido");
+        }
+
         this.titulo = titulo;
     }
 
     public void setAutor(String autor) {
+        if (contieneNulo(autor)) {
+            throw new NullPointerException("Autor no válido");
+        }
         this.autor = autor;
     }
 
     public void setNumeroPaginas(int numeroPaginas) {
+    if (numeroPaginas<=0){
+        throw new IllegalArgumentException("Número de páginas  válido");
+    }
+
         this.numeroPaginas = numeroPaginas;
     }
 
     public void setEditorial(String editorial) {
+        if (contieneNulo(editorial)) {
+            throw new NullPointerException("editorial no válida");
+        }
+
         this.editorial = editorial;
     }
 
@@ -77,4 +103,22 @@ public Libro(long isbn, String titulo, String autor, int numeroPaginas, String e
     public String toString() {
         return "Título: " + titulo + " - ISBN: " + isbn + " - AUTOR: "+ autor + " - PÁGINAS=" + numeroPaginas;
     }
+
+    private boolean contieneDigitos (String isbn){
+        for (int i = 0; i < isbn.length() ; i++) {
+            if (!Character.isDigit(isbn.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean contieneNulo (String prueba){
+     if (prueba==null){
+         return true;
+     }
+     return false;
+    }
+
+
 }
--- PROGRAMACION/Segundo trimestre/ISBN/src/Main.java
+++ PROGRAMACION/Segundo trimestre/ISBN/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan= new Scanner(System.in);
+
+        try{
+            Libro libro1= new Libro("123456789009", "20000 leguas de viaje Submarino", "Julio Verne", 40, "Tauro");
+
+            Libro libro2= new Libro("123789009", "20000 leguas de viaje Submarino", "Julio Verne");
+
+            Libro libro3= new Libro("123789009", "20000 leguas de viaje Submarino", "4");
+
+
+
+        } catch (NullPointerException | IllegalArgumentException | InputMismatchException e) {
+            System.err.println("Error " + e.getMessage());
+        }
+
+
+
+
+
+
     }
 }
